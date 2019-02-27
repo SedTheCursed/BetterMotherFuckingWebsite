9bb5ffcd058a261ee21660f639c4ee4407c4c6fd
SedTheCursed <sed.robert@gmail.com>
2019-02-27 14:48:31 +0100

    Ajout du bouton permettant de changer de thème

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 setTimeout(() => {
-    if (confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please")) {
+    if (confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please\nVeux-tu aller sur xxxvidsxxx ?")) {
         document.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
     }
 },10000);
+
+document.querySelector("#psyche").addEventListener("click", (e) => {
+    document.body.classList.add("psy");
+    e.target.style.display = "none";
+    document.getElementById("normal").style.display= "initial";
+});
+
+document.getElementById("normal").addEventListener("click", (e) => {
+    document.body.classList.remove("psy");
+    e.target.style.display = "none";
+    document.getElementById("psyche").style.display= "initial";
+});
