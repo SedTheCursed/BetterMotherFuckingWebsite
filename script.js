fc59a1b1271e90a8ef70cbc35223da1b6669f34c
SedTheCursed <sed.robert@gmail.com>
2019-02-27 13:52:11 +0100

    Amelioration de la popup, avec une redirection.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 setTimeout(() => {
-    alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please")
+    if (confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please")) {
+        document.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    }
 },10000);
